ba536c102bd97d98a4ef808fd81ddae8b49c174b
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-10-31 13:09:10 +0100

    Added database context

---
+++ StudentRegistrationDBDEMO/StudentDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentRegistrationDBDEMO
+{
+    internal class StudentDbContext : DbContext
+    {
+        private readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=StudentDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    
+        public DbSet<Student>? Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
