823f35e1264aa1b40435ec189a6d9c82be28421b
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-10-31 15:56:59 +0100

    Added documentation to student class

--- StudentRegistrationDBDEMO/Student.cs
+++ StudentRegistrationDBDEMO/Student.cs
@@ -7,12 +7,24 @@ namespace StudentRegistrationDBDEMO
         [Key]
         public int StudentId { get; private set; }
 
+        /// <summary>
+        /// The student's first name
+        /// </summary>
+        /// <remarks>
+        /// Property cannot be null or empty, nor contain any whitespace characters
+        /// </remarks>
         [Required(ErrorMessage = "First name is required.")]
         [RegularExpression(@"^\S+$", ErrorMessage = "First name cannot contain spaces")]
         public string? StudentFirstName { get; set; }
 
+        /// <summary>
+        /// The student's last name
+        /// </summary>
         public string? StudentLastName { get; set; }
 
+        /// <summary>
+        /// The student's city of residence
+        /// </summary>
         [Required(ErrorMessage = "City is required.")]
         public string? StudentCity { get; set; }
     }
